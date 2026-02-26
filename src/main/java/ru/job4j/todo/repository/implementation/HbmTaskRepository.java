98e1983e5e8677971d38457a31ddefd2820707ee
corsair <corsair954@mail.ru>
2026-02-26 18:47:05 +0300

    6. Лямбды и шаблон Command [#49295]

--- src/main/java/ru/job4j/todo/repository/implementation/HbmTaskRepository.java
+++ src/main/java/ru/job4j/todo/repository/implementation/HbmTaskRepository.java
@@ -1,127 +1,66 @@
 package ru.job4j.todo.repository.implementation;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Task;
 import ru.job4j.todo.repository.TaskRepository;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 @AllArgsConstructor
 public class HbmTaskRepository implements TaskRepository {
 
-    private final SessionFactory sf;
+    private final CrudRepository crudRepository;
 
     @Override
     public List<Task> findUnFinishedTasks() {
-        Session session = sf.openSession();
-        Query<Task> query = session.createQuery("FROM Task t WHERE t.done = FALSE", Task.class);
-        var result = query.getResultList();
-        session.close();
-        return result;
+        return crudRepository.query("FROM Task t WHERE t.done = FALSE", Task.class);
     }
 
     @Override
     public List<Task> findFinishedTasks() {
-        Session session = sf.openSession();
-        Query<Task> query = session.createQuery("FROM Task t WHERE t.done = TRUE", Task.class);
-        var result = query.getResultList();
-        session.close();
-        return result;
+        return crudRepository.query("FROM Task t WHERE t.done = TRUE", Task.class);
     }
 
     @Override
     public Optional<Task> findById(int id) {
-        Session session = sf.openSession();
-        Query<Task> query = session.createQuery("FROM Task t WHERE t.id = :fid", Task.class)
-                .setParameter("fid", id);
-        var result = query.uniqueResultOptional();
-        session.close();
-        return result;
+        return crudRepository.optional("FROM Task t WHERE t.id = :fid", Task.class, Map.of("fid", id));
     }
 
     @Override
     public Task addTask(Task task) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            task.setCreated(LocalDateTime.now());
-            session.save(task);
-            session.getTransaction().commit();
-            session.close();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            throw new RuntimeException(e.getMessage());
-        }
+        task.setCreated(LocalDateTime.now());
+        crudRepository.run(session -> session.save(task));
         return task;
     }
 
     @Override
     public boolean delete(Integer id) {
-        Session session = sf.openSession();
-        int result = 0;
-        try {
-            session.beginTransaction();
-            result = session.createQuery("DELETE Task t WHERE t.id = :tid")
-                    .setParameter("tid", id)
-                    .executeUpdate();
-            session.getTransaction().commit();
-            session.close();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-        return result > 0;
+        return crudRepository.run("DELETE Task t WHERE t.id = :tid", Map.of("tid", id)) > 0;
     }
 
     @Override
     public List<Task> findAll() {
-        Session session = sf.openSession();
-        Query<Task> query = session.createQuery("FROM Task", Task.class);
-        var result = query.getResultList();
-        session.close();
-        return result;
+        return crudRepository.query("FROM Task", Task.class);
     }
 
     @Override
     public boolean makeDone(Integer id) {
-            Session session = sf.openSession();
-            int result = 0;
-            try {
-                session.beginTransaction();
-                result = session.createQuery("UPDATE Task  SET done = TRUE WHERE id = :fid")
-                        .setParameter("fid", id)
-                        .executeUpdate();
-                session.getTransaction().commit();
-                session.close();
-            } catch (Exception e) {
-                session.getTransaction().rollback();
-            }
-        return result > 0;
+        return crudRepository.run("UPDATE Task  SET done = TRUE WHERE id = :fid", Map.of("fid", id)) > 0;
     }
 
     @Override
     public boolean update(Task task) {
-        Session session = sf.openSession();
-        int result = 0;
-        try {
-            session.beginTransaction();
-            result = session.createQuery("UPDATE Task t SET t.title = :fTitle, t.description = :fDesc, t.created = :fCreated, t.done = :fDone WHERE t.id = :fid")
-                    .setParameter("fTitle", task.getTitle())
-                    .setParameter("fDesc", task.getDescription())
-                    .setParameter("fid", task.getId())
-                    .setParameter("fDone", task.isDone())
-                    .executeUpdate();
-            session.getTransaction().commit();
-            session.close();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-        return result > 0;
+        var map = new HashMap<String, Object>();
+        map.put("fTitle", task.getTitle());
+        map.put("fDesc", task.getDescription());
+        map.put("fDone", task.isDone());
+        map.put("fid", task.getId());
+        return crudRepository.run("UPDATE Task t SET t.title = :fTitle, t.description = :fDesc, t.done = :fDone WHERE t.id = :fid", map) > 0;
     }
 }
--- src/main/java/ru/job4j/todo/repository/implementation/HbmUserRepository.java
+++ src/main/java/ru/job4j/todo/repository/implementation/HbmUserRepository.java
@@ -1,10 +1,6 @@
 package ru.job4j.todo.repository.implementation;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.exception.ConstraintViolationException;
-import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
@@ -12,37 +8,32 @@ import ru.job4j.todo.model.User;
 import ru.job4j.todo.repository.UserRepository;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Optional;
 
 @Repository
 @AllArgsConstructor
 public class HbmUserRepository implements UserRepository {
-    private final SessionFactory sf;
+    private final CrudRepository crudRepository;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HbmUserRepository.class);
 
     @Override
     public Optional<User> findByLoginAndPassword(String login, String password) {
-        Session session = sf.openSession();
-        Query<User> query = session.createQuery("FROM User u WHERE u.login = :flogin AND u.password = :fpassword", User.class)
-                .setParameter("flogin", login)
-                .setParameter("fpassword", password);
-        var result = query.uniqueResultOptional();
-        session.close();
-        return result;
+        var map = new HashMap<String, Object>();
+        map.put("flogin", login);
+        map.put("fpassword", password);
+        return crudRepository.optional("FROM User u WHERE u.login = :flogin AND u.password = :fpassword",
+                User.class,
+                map);
     }
 
     @Override
     public Optional<User> save(User user) {
-        Session session = sf.openSession();
         try {
-            session.beginTransaction();
-            session.save(user);
-            session.getTransaction().commit();
-            session.close();
+            crudRepository.run(session -> session.save(user));
             return Optional.of(user);
         } catch (Exception e) {
-            session.getTransaction().rollback();
             var cause = e.getCause();
             if (cause instanceof SQLException sqlEx) {
                 if ("23505".equals(sqlEx.getSQLState())) {
